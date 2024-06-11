1eacac33c577d872e26246db9d241681c38f2abc
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-06-11 17:43:19 +0530

    feature_add: created componenets realted to dashboard

---
+++ src/components/Dashboard/Pagination/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles.css";
+
+import Pagination from "@mui/material/Pagination";
+
+export default function PaginationComponent({ page, handlePageChange }) {
+  return (
+    <div className="pagination-div">
+      <Pagination
+        sx={{
+          "& .MuiPaginationItem-text": {
+            color: "var(--white) !important",
+            border: "2px solid var(--grey)",
+          },
+          "& .MuiPaginationItem-text:hover": {
+            backgroundColor: "transparent !important",
+            color: "var(--white) !important"
+
+          },
+          "& .Mui-selected  ": {
+            backgroundColor: "var(--blue)",
+            borderColor: "var(--white)",
+            color: "var(--white) !important"
+          },
+          "& .MuiPaginationItem-ellipsis": {
+            border: "none",
+          },
+        }}
+        count={10}
+        page={page}
+        onChange={handlePageChange}
+      />
+    </div>
+  );
+}
