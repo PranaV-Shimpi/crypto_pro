3def9fdf25c6bfac41f03e7f9eb2964ad0679c50
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-06-11 18:33:01 +0530

    add: add animation to moveToTop Button

--- src/components/common/TopButton/index.js
+++ src/components/common/TopButton/index.js
@@ -1,34 +1,39 @@
-import React from "react";
-import ExpandLessRoundedIcon from "@mui/icons-material/ExpandLessRounded";
+import React, { useEffect } from "react";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import "./styles.css"; 
+
 function TopButton() {
-  // Get the button
-  let mybutton = document.getElementById("top-btn");
+  useEffect(() => {
+    const mybutton = document.getElementById("top-btn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
+    const scrollFunction = () => {
+      if (
+        document.body.scrollTop > 500 ||
+        document.documentElement.scrollTop > 500
+      ) {
+        mybutton.style.display = "flex";
+      } else {
+        mybutton.style.display = "none";
+      }
+    };
 
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 500 ||
-      document.documentElement.scrollTop > 500
-    ) {
-      mybutton.style.display = "flex";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
+    window.addEventListener("scroll", scrollFunction);
+
+    return () => {
+      window.removeEventListener("scroll", scrollFunction);
+    };
+  }, []);
+
   return (
     <div
-      className="top-btn"
+      className="topp-btn"
       id="top-btn"
       onClick={() => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
       }}
     >
-      <ExpandLessRoundedIcon />
+      <KeyboardDoubleArrowUpIcon />
     </div>
   );
 }
